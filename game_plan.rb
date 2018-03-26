a6cff3942c39479964403042def097c565530c89
Renato Amaral <srkimbo@gmail.com>
2018-03-26 19:44:11 -0400

    Class files and Game plan created

---
+++ game_plan.rb
@@ -0,0 +1,29 @@
+
+# classes in the game
+
+# Players class
+
+  # has no user input
+  # has only static information
+  # has Player number and number of lives
+  # variables store Player numbers
+  # variable store starlives
+
+# Game class - pass the players as parameters
+
+  # changes managed here - lives left
+  # has user input
+  # has Turn
+  # variable stores Turn number
+  # method: calculates how many lives the player has left
+  # method: figures out who's turn it is - set current player and switch ever time
+  # method: game loop (where each instance of the loop is the other players turn)
+  # method: to find when game is over
+
+# Question class
+
+  # Holds All questions
+  # Randonly pick questions
+  # Math behind the Questions
+    # method: creates a math question based on equation
+    # Evaluate if answer is correct
